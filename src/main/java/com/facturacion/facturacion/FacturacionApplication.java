d29556036491f03951f4586e84460fe0728cd1b0
titis28 <alma.gonzalez@cua.uam.mx>
2025-04-26 20:22:04 -0600

    Cambios en Application

--- src/main/java/com/facturacion/facturacion/FacturacionApplication.java
+++ src/main/java/com/facturacion/facturacion/FacturacionApplication.java
@@ -2,31 +2,16 @@ package com.facturacion.facturacion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
 @SpringBootApplication
 public class FacturacionApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(FacturacionApplication.class, args);
-
-		FacturaRepository repo = context.getBean(FacturaRepository.class);
-
-		DetalleFactura detalle = new DetalleFactura(2, "Cuaderno", 3, 22.5);
-		Factura factura = new Factura();
-			factura.setFecha(LocalDateTime.now());
-		factura.setProductos(List.of(detalle));
-		factura.calcularTotales();
-
-		repo.save(factura);
-		System.out.println("Factura creada desde main");
+		SpringApplication.run(FacturacionApplication.class, args);
 	}
 
 	
